9a0702ea78364d6e13afad15fa3c3472bdee57d9
Johannes van Schalkwyk <jvs@nimbledesign.co.za>
2017-12-28 17:39:13 +0200

    Add GET /todos/:id code & test cases

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,12 +1,15 @@
 const expect = require('expect');
 const request = require('supertest');
+const {ObjectID} = require('mongodb'); // ES6 destructuring
 
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
 const todos = [{
+  _id: new ObjectID(),
   text: 'First test todo'
 }, {
+  _id: new ObjectID(),
   text: 'Second test todo'
 }];
 
@@ -20,7 +23,7 @@ describe('*** POST /todos', () => {
   it('should create a new todo', (done) => {
     var text = 'Test todo text';
 
-    request(app)
+    request(app) // supertest
       .post('/todos')
       .send({text})
       .expect(200)
@@ -41,7 +44,7 @@ describe('*** POST /todos', () => {
   });
 
   it('should not create todo with invalid body data', (done) => {
-    request(app)
+    request(app) // supertest
       .post('/todos')
       .send({})
       .expect(400)
@@ -60,12 +63,37 @@ describe('*** POST /todos', () => {
 
 describe('*** GET /todos', () => {
   it('should get all todos', (done) => {
-    request(app)
+    request(app) // supertest
       .get('/todos')
       .expect(200)
       .expect((res) => {
         expect(res.body.todos.length).toBe(2);
       })
+      .end(done);
+  });
+});
+
+describe('GET /todos/:id', () => {
+  it('should return todo document', (done) => {
+    request(app) // supertest
+      .get(`/todos/${todos[0]._id.toHexString()}`)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo.text).toBe(todos[0].text);
+      })
+      .end(done);
+  });
+  it('should return 404 if todo is not found', (done) => {
+    var hexId = new ObjectID().toHexString();
+    request(app) //supertest
+      .get(`/todos/${hexId}`)
+      .expect(404)
+      .end(done);
+  });
+  it('should return 404 for _ids with an invalid format', (done) => {
+    request(app) //supertest
+      .get('/todos/123abc') //invalid format _id
+      .expect(404)
       .end(done);
   });
 });
